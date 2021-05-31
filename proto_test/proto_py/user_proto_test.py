b83df11356a689fe1cc25b41df3f824ed8a56b84
sinlov <sinlovgmppt@gmail.com>
2021-05-31 14:37:16 +0800

    add websocket-client at Pipfile add test_user_pb_deserialization

--- proto_test/proto_py/user_proto_test.py
+++ proto_test/proto_py/user_proto_test.py
@@ -9,7 +9,7 @@ import json
 
 class UserProtoPyTest(unittest.TestCase):
 
-    def test_user_base(self):
+    def test_user_pb_serialization(self):
         fake = Faker()
         fake_name = fake.name()
         fake_email = fake.email()
@@ -33,6 +33,31 @@ class UserProtoPyTest(unittest.TestCase):
         self.assertEqual(json_data['phoneNumber']['number'], fake_phone_number)
         self.assertNotEqual(json_data['phoneNumber']['type'], PhoneNumber.MOBILE)
         self.assertEqual(json_data['phoneNumber']['type'], PhoneNumber.PhoneType.Name(PhoneNumber.MOBILE))
+
+    def test_user_pb_deserialization(self):
+        fake = Faker()
+        fake_name = fake.name()
+        fake_email = fake.email()
+        fake_phone_number = fake.phone_number()
+
+        user_pb = User.User()
+        user_pb.uid = str(uuid.uuid4())
+        user_pb.name = fake_name
+        user_pb.email = fake_email
+        user_pb.age = 16
+        user_pb.phoneNumber.number = fake_phone_number
+        user_pb.phoneNumber.type = PhoneNumber.HOME
+
+        pb_serialize_to_string = user_pb.SerializeToString()
+        print('user_pb.SerializeToString:\n{0}'.format(pb_serialize_to_string))
+
+        user_ds = User.User()
+        user_ds.ParseFromString(pb_serialize_to_string)
+        self.assertEqual(user_ds.name, fake_name)
+        self.assertEqual(user_ds.email, fake_email)
+        self.assertEqual(user_ds.phoneNumber.number, fake_phone_number)
+        self.assertEqual(user_ds.phoneNumber.type, PhoneNumber.HOME)
+        pass
 
 
 if __name__ == '__main__':
